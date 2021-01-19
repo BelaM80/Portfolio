26ed7e939866b81c7f29a341b0d10df9192fec01
BelaM80 <mueller.bela@gmx.de>
2021-01-19 10:42:25 +0100

    Add fix to links

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -35,9 +35,9 @@ const Contact = () => {
             <h1>Contact</h1>
             <Div>
                <a href="mailto: mueller.bela@gmx.de" ><img src={email} alt="email"/></a>
-                <a href="https://github.com/BelaM80" target="_blank"><img src={github} alt="github"/></a>
-                <a href="https://www.xing.com/profile/BelaLars_Mueller/cv" target="_blank"><img src={xing} alt="xing"/></a>
-                <a href="https://www.linkedin.com/in/b%C3%A9la-m%C3%BCller-580109164/" target="_blank"><img src={linkedin} alt="linkedin"/></a>
+                <a href="https://github.com/BelaM80" target="_blank" rel="noreferrer"><img src={github} alt="github"/></a>
+                <a href="https://www.xing.com/profile/BelaLars_Mueller/cv" target="_blank" rel="noreferrer"><img src={xing} alt="xing"/></a>
+                <a href="https://www.linkedin.com/in/b%C3%A9la-m%C3%BCller-580109164/" target="_blank" rel="noreferrer"><img src={linkedin} alt="linkedin"/></a>
 
             </Div>
 
