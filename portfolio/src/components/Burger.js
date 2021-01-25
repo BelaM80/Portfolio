89af73a5a9f989456cb0044721f376b79daaf120
BelaM80 <mueller.bela@gmx.de>
2021-01-25 16:39:46 +0100

    Create burger menue animation

--- portfolio/src/components/Burger.js
+++ portfolio/src/components/Burger.js
@@ -26,10 +26,22 @@ const StyledBurger = styled.button`
   div {
     width: 2rem;
     height: 0.25rem;
-    background: var(--primary-color);
+    background: ${({  open }) => open ? "var(--secondary-color)" : "var(--primary-color)"};
     border-radius: 10px;
     position: relative;
     transform-origin: 1px;
+    :first-child {
+      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => open ? '0' : '1'};
+      transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+    }
   }
 
 `;
